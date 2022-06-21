4ae3928724eddf5e5117cdb9d3bd629e7d848f7b
Philip Ifeanyi <akubuiro.ifeanyi.philip@gmail.com>
2022-06-21 23:35:35 +0100

    JavaScript Beginner, learnt the document model

---
+++ helloWorld.js
@@ -0,0 +1,7 @@
+var namae = document.getElementById('name');
+var height = document.getElementById('height');
+var country = document.getElementById('country');
+
+namae.innerHTML = '<b>Name:</b> My name is Philip Ifeanyi'
+height.innerHTML = '<b>Height:</b> I am a couple foot tall, defs not up to 7 though'
+country.innerHTML = '<b>Country:</b> I am Nigerian'
